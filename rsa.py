28e3dcb3771f59b1d3fc2225f7ad65efa74f7509
dlund1010 <lundev@sonoma.edu>
2025-09-30 18:41:50 -0700

    Problem 4 and 5 pseudocode

--- rsa.py
+++ rsa.py
@@ -1,7 +1,7 @@
 import random
-from primality import *
 from random_prime_generator import generate_random_prime
-from gcd import gcd 
+from primality import modexp
+from gcd import gcd
 
 def find_inverse(a, b):
     x, y, d = e_gcd(b, a)
@@ -18,27 +18,64 @@ def e_gcd(x, y):
     return y2, x2 - (x//y * y2), d
 
 # problem 4 main function
-def generate_rsa_keys(bits, k):
+# inputs:
+#   bits (number of bits prime factors should be)
+#   precision (precision used by prime generator)
+# pre-conditions:
+#   bits and precision are positive integers
+# outputs:
+#   factor1, factor2 (prime factors of modulus key)
+#   mod (modulus key)
+#   e_key, d_key (encryption and decryption exponents)
+# pseudocode:
+#   get 2 prime factors, f1, f2
+#   modulus = f1*f2
+#   exp_mod = (f1 - 1) * (f2 - 1)
+#   loop:
+#       generate random 10 bit integer, e
+#       if gcd(e, exp_mod) = 1
+#           d = modular inverse of e wrt. exp_mod
+#           if d != 1
+#               break out of loop
+#   return f1, f2, modulus, e, d
+
+def generate_rsa_keys(bits, precision):
     # generate primes and compute mods to be used
-    factor1 = generate_random_prime(bits, k)
-    factor2 = generate_random_prime(bits, k)
+    factor1 = generate_random_prime(bits, precision)
+    factor2 = generate_random_prime(bits, precision)
     mod = factor1 * factor2
-    key_mod = (factor1 - 1) * (factor2 - 1)
+    exp_mod = (factor1 - 1) * (factor2 - 1)
 
     while True:
         # generate random (exactly) 10 bit integer
         e_key = random.randint(512, 1023)
-        # test validity, must be relatively prime to key_mod and have an inverse other than 1
-        if (gcd(e_key, key_mod) == 1):
-            d_key = find_inverse(e_key, key_mod)
+        # test validity
+        # must be relatively prime to exp_mod and have an inverse other than 1
+        if (gcd(e_key, exp_mod) == 1):
+            d_key = find_inverse(e_key, exp_mod)
             if (d_key == 1):
                 continue
             else:
                 return factor1, factor2, mod, e_key, d_key
 
 # problem 5 main function
-def rsa(mod, e_key, d_key, message):
-    encrypted = modexp(message, e_key, mod)
-    decrypted = modexp(encrypted, d_key, mod)
+# inputs:
+#   mod_key (modulus key)
+#   e_key, d_key (encryption and decryption exponents)
+#   message
+# pre-conditions:
+#   mod_key is the product of 2 prime numbers
+#   e_key and d_key are exponent inverses
+#   (meaning (a^e_key)^d_key = a)
+#   message is non-negative and less than mod_key
+# outputs:
+#   encrypted message and decrypted message
+# pseudocode:
+#   encrypted = m^e_key mod mod_key
+#   decrypted = encrypted^d_key mod mod_key
+
+def rsa(mod_key, e_key, d_key, message):
+    encrypted = modexp(message, e_key, mod_key)
+    decrypted = modexp(encrypted, d_key, mod_key)
 
     return encrypted, decrypted
